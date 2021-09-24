2a0b33cec6798a3ef5dd7cae1c9383ea2a1942ad
renesat <smol.ivan97@gmail.com>
2021-09-24 07:45:25 +0300

    Fix and add test in tlnet

--- tlnet/model/tlnet.py
+++ tlnet/model/tlnet.py
@@ -1,8 +1,8 @@
+import numpy as np
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 from torch import nn
-import numpy as np
 
 
 class TLNet(pl.LightningModule):
@@ -74,22 +74,61 @@ class TLNet(pl.LightningModule):
                 reg_loss += 2 * ((box_pred[i] - box[i]) ** 2).sum().sqrt()
         loss = reg_loss + bse_loss
 
-        accuracy = ((cls_pred > 0.5) == (cls > 0.5))
+        accuracy = (cls_pred > 0.5) == (cls > 0.5)
 
         self.log("val/loss", loss)
         self.log("val/reg_loss", reg_loss)
         self.log("val/bse_loss", bse_loss)
 
         return {
-                "loss":loss,
-                "accuracy": accuracy,
-                }
+            "loss": loss,
+            "accuracy": accuracy,
+        }
 
     def validation_epoch_end(self, outputs):
-       accuracy = []
-       for out in outputs:
-           accuracy += list(out["accuracy"].detach().cpu().numpy())
-       self.log("val/accuracy", np.mean(accuracy))
+        accuracy = []
+        for out in outputs:
+            accuracy += list(out["accuracy"].detach().cpu().numpy())
+        self.log("val/accuracy", np.mean(accuracy))
+
+    def test_step(self, batch, batch_idx):
+        img = batch["image"]
+        cls = batch["class"]
+        box = batch["box"]
+
+        cls_pred, box_pred = self.forward(img)
+
+        accuracy = (cls_pred > 0.5) == (cls > 0.5)
+        tp = ((cls_pred[cls > 0.5] > 0.5) == (cls[cls > 0.5] > 0.5)).sum()
+        fp = ((cls_pred[cls > 0.5] > 0.5) != (cls[cls > 0.5] > 0.5)).sum()
+        fn = ((cls_pred[cls <= 0.5] > 0.5) != (cls[cls <= 0.5] > 0.5)).sum()
+
+        return {
+            "count": len(cls),
+            "fn": fn,
+            "fp": fp,
+            "tp": tp,
+            "accuracy": accuracy,
+        }
+
+    def test_epoch_end(self, outputs):
+        accuracy = []
+        tp = 0
+        fp = 0
+        fn = 0
+        count = 0
+        for out in outputs:
+            accuracy += list(out["accuracy"].detach().cpu().numpy())
+            tp += out["tp"].detach().cpu().item()
+            fp += out["fp"].detach().cpu().item()
+            fn += out["fn"].detach().cpu().item()
+            count += out["count"]
+        tp_rate = tp / (tp + fp)
+        print("TP Rate:", tp_rate)
+        print("FN:", fn / count)
+
+    def test_dataloader(self):
+        pass
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
